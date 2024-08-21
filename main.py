13d524a14735f3760f22813880f730a6269c5605
IT21314742 <it21314742@my.sliit.lk>
2024-08-21 20:07:23 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-BGR_color = np.array([[[255, 0, 0]]])
-x = cv2.cvtColor([[[255, 0, 0]]], cv2.COLOR_BGR2HSV)
-x[0][0]
+# BGR_color = np.array([[[255, 0, 0]]])
+# x = cv2.cvtColor([[[255, 0, 0]]], cv2.COLOR_BGR2HSV)
+# x[0][0]
